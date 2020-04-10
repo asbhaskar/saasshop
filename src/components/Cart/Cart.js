f9b7408f707c0348e00ab8711a048592911eab81
Rachel Li <rachelrli@berkeley.edu>
2020-04-09 23:23:21 -0400

    create subfolders for components

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import CartItem from "./CartItem/CartItem";
+import "./Cart.css";
+
+class Cart extends Component {
+  state = {};
+
+  calculateTotal = () => {
+    var total = 0;
+    for (var i = 0; i < this.props.items.length; i++) {
+      total = total + this.props.items[i].price * this.props.items[i].value;
+    }
+    return total;
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Your Shopping Cart</h1>
+        <div className="cartContainer">
+          <div className="cartSummary">
+            {this.props.items.map((item) => (
+              <CartItem
+                key={item.id}
+                item={item}
+                onIncrement={this.props.onIncrement}
+                onDecrement={this.props.onDecrement}
+                onDelete={this.props.onDelete}
+              />
+            ))}
+          </div>
+          <div className="paymentBox">
+            <p style={{ textAlign: "right" }}>
+              <strong>SUBTOTAL</strong> ${this.calculateTotal()}
+            </p>
+            <form>
+              <label for="name">Name</label>
+              <input type="text" id="name" name="name"></input>
+              <br></br>
+              <label for="email">Email</label>
+              <input type="text" id="email" name="email"></input>
+              <br></br>
+              <div className="paymentMethod">
+                <label for="payment">Payment Method</label>
+                <input
+                  type="radio"
+                  id="venmo"
+                  name="payment"
+                  value="venmo"
+                ></input>
+                <label for="venmo">Venmo</label>
+
+                <input
+                  type="radio"
+                  id="cash"
+                  name="payment"
+                  value="cash"
+                ></input>
+                <label for="cash">Cash</label>
+
+                <input
+                  type="radio"
+                  id="card"
+                  name="payment"
+                  value="card"
+                ></input>
+                <label for="card">Card</label>
+                <br></br>
+              </div>
+              <label for="cardNumber">Card Number</label>
+              <input type="text" id="cardNumber" name="cardNumber"></input>
+              <br></br>
+              <label for="cardName">Name on Card</label>
+              <input type="text" id="cardName" name="cardName"></input>
+              <br></br>
+              <label for="expiration">Expiration Date</label>
+              <input type="text" id="expiration" name="expiration"></input>
+              <br></br>
+              <p>
+                If you selected Venmo, please Venmo ${this.calculateTotal()} to
+                @calsaas.
+              </p>
+              <input className="submit" type="submit" value="Submit"></input>
+            </form>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Cart;
---
+++ src/containers/ShopHome.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import NavBar from "../components/NavBar/NavBar";
+import Shop from "../components/Shop/Shop";
+import Cart from "../components/Cart/Cart";
+import "./ShopHome.css";
+import Zoomer from "../assets/images/shirts/zoomer.png";
+import Logo from "../assets/images/stickers/SAAS_logo.png";
+
+class ShopHome extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        value: 0,
+        name: "Ok Zoomer",
+        price: 20,
+        image: Zoomer,
+      },
+      {
+        id: 2,
+        value: 0,
+        name: "SAAS Logo",
+        price: 2,
+        image: Logo,
+      },
+    ],
+  };
+
+  handleIncrement = (item) => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index] = { ...item };
+    items[index].value++;
+    this.setState({ items });
+  };
+
+  handleDecrement = (item) => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index] = { ...item };
+    if (items[index].value > 0) {
+      items[index].value--;
+    }
+    this.setState({ items });
+  };
+
+  handleDelete = (item) => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index] = { ...item };
+    items[index].value = 0;
+    this.setState({ items });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar items={this.state.items.filter((i) => i.value > 0)} />
+        <main className="container">
+          <Shop
+            items={this.state.items}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+          />
+        </main>
+        <main className="container">
+          <Cart
+            items={this.state.items.filter((i) => i.value > 0)}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ShopHome;

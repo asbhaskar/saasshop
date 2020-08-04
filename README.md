4231167e8767145d992d02f4cd0f4af705453a93
Sheepinav <abhaskar@berkeley.edu>
2020-08-03 22:34:28 -0500

    adding basic server and changing directory structure, read README.md for instructions on running

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 ## The Official Student Association for Applied Statistics's Merchandise Shop
-## Made By Abhinav Bhaskar, Rachel Li, Valerie Yip
+## Made By Abhinav Bhaskar, Rachel Li, Valerie Yip  
 
-This project was a collaboration between three generations of SAAS's Web Development Directors, created during the semester of COVID-19 in an effort to streamline our merchandise selling process.
+This project was a collaboration between three generations of SAAS's Web Development Directors, created during the semester of COVID-19 in an effort to streamline our merchandise selling process.  
 
-The project thus far includes basic features of an ecommerce site. A admin page and user home will be added in the future.
+The project thus far includes basic features of an ecommerce site. A admin page and user home will be added in the future.  
 
-The project uses React, Redux, Firebase, Stripe API, ES6+
+The project uses React, Redux, Firebase, Stripe API, ES6+  
 
-*Note the items displayed are not actually for sale*
+*Note the items displayed are not actually for sale*  
 
+## Usage
+There are two package.json files, one in the root directory and one inside client/. Install both sets of dependencies.  
+
+Run `npm run build` inside the root directory. This will cd inside the client folder and build the frontend. The build folder will be omitted from github due to size.  
+Then you will be able to start the server by typing `npm run dev` again in the root directory which will run the server at http:/localhost:5000.  
+When adding a stripe API Key, create a `.env` file and paste `STRIPE_SECRET_KEY=YOUR_SECRET_KEY_ID`. Keys should not be stored on git for security reasons.
+
+## Images
 ![Home page](./images/shop-home.png)
 
 ![Checkout page](./images/checkout.png)
---
+++ client/build/precache-manifest.d8bde7a1060bb02fedadc4e50b63fae6.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0af7a412b39da98e4ee11f7168b76a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad89039829dbfa464013",
+    "url": "/static/css/2.59b7d1eb.chunk.css"
+  },
+  {
+    "revision": "442ebc8047122ae9b7d8",
+    "url": "/static/css/main.7f50161d.chunk.css"
+  },
+  {
+    "revision": "ad89039829dbfa464013",
+    "url": "/static/js/2.ab1f7ed4.chunk.js"
+  },
+  {
+    "revision": "265bd25f7d67aeb3a3ddf1b77cc6c926",
+    "url": "/static/js/2.ab1f7ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442ebc8047122ae9b7d8",
+    "url": "/static/js/main.11433ba3.chunk.js"
+  },
+  {
+    "revision": "8934b33b92b66eecb0ca",
+    "url": "/static/js/runtime-main.dbf998bc.js"
+  },
+  {
+    "revision": "dea4696108a6be8c039c99309ad3a610",
+    "url": "/static/media/SAAS_logo.dea46961.png"
+  },
+  {
+    "revision": "a4de093f205d929fcbd2ed23a95a0ef9",
+    "url": "/static/media/gradient.a4de093f.jpg"
+  },
+  {
+    "revision": "dea4696108a6be8c039c99309ad3a610",
+    "url": "/static/media/logo.dea46961.png"
+  },
+  {
+    "revision": "b7dd9980b1b94c3d567aaa4e791a4b22",
+    "url": "/static/media/logo_merch.b7dd9980.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "bf35cb6c0c3cf8024fd55986f637a459",
+    "url": "/static/media/supermarket.bf35cb6c.svg"
+  },
+  {
+    "revision": "2d239e463293cb2468ce7ab9ddfbef87",
+    "url": "/static/media/user.2d239e46.svg"
+  },
+  {
+    "revision": "08b1e1ade2ae5e87c4eec1cee7ed29b1",
+    "url": "/static/media/zoomer.08b1e1ad.png"
+  }
+]);
